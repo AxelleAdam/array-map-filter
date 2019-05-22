f2060037f039e319ff4f694c371b0939e03aa324
Axelle Adamkiewicz <axelle.adamkiewicz@gmail.com>
2019-05-22 11:58:31 +0200

    Adding my filter exercices.

--- filter/ex3.js
+++ filter/ex3.js
@@ -1,13 +1,9 @@
 /* Array.prototype.filter - Exercice 3
-
 Ecrire une fonction keepStarks, qui prend en arguments un tableau de chaînes
 (plus spécifiquement, des noms de personnages de Game of Thrones).
-
 La fonction doit renvoyer un tableau ne contenant que les membres de la famille Stark.
-
 Il y a plusieurs façons d'écrire cette fonction, et outre filter, tu auras probablement
 besoin d'une de ces méthodes de String: endsWith ou split.
-
 Exemple d'entrée:
   [
     'Bran Stark',
@@ -17,14 +13,24 @@ Exemple d'entrée:
     'Yara Greyjoy',
     'Sansa Stark'
   ]
-
 Sortie attendue:
   ['Bran Stark', 'Arya Stark', 'Sansa Stark']
-
  */
 
 function keepStarks(names) {
-}
+  return (names.filter((entry) => {
+    return entry.endsWith('Stark');
+  }));
+};
+
+keepStarks([
+  'Bran Stark',
+  'Cersei Lannister',
+  'Sandor Clegane',
+  'Arya Stark',
+  'Yara Greyjoy',
+  'Sansa Stark'
+]);
 
 // Ne pas modifier l'export
 module.exports = keepStarks;
--- filter/ex5.js
+++ filter/ex5.js
@@ -1,17 +1,13 @@
 /* Array.prototype.filter - Exercice 5
-
 Ecrire une fonction searchWordFilter, qui attends deux paramètres:
 1. un tableau de chaînes (par exemple des noms de produits, de films, etc.)
 2. un terme de recherche
-
 La fonction doit renvoyer un tableau ne contenant que les éléments dont le nom contient
 la chaîne recherchée.
-
 Il y a là aussi plusieurs façons d'écrire cette fonction, en utilisant indexOf ou includes.
 Une petite difficulté supplémentaire: la recherche être "case insensitive" (insensible à la
 casse), c'est-à-dire qu'une recherche sur 'star' ne doit pas éliminer les chaînes contenant
 'Star'.
-
 Exemple d'entrée:
   
 1. tableau de chaînes
@@ -24,22 +20,31 @@ Exemple d'entrée:
     'Star Wars: Episode II - Attack of the Clones',
     'Star Wars: Episode III - Revenge of the Sith'
   ]
-
 2. terme de recherche: 'revenge'
-
-
 ==> Sortie attendue:
   [
     'Revenge of the Nerds',
     'Revenge of the Pink Panther',
     'Star Wars: Episode III - Revenge of the Sith'
   ]
-
-
  */
 
 function searchWordFilter(items, search) {
+  return (items.filter((entry) => {
+    let entryLowerCase = entry.toLowerCase();
+    let index = entryLowerCase.indexOf(search);
+    return entry[index];
+  }));
 }
 
+searchWordFilter([
+  'Mad Max: Fury Road',
+  'Interstellar',
+  'Revenge of the Nerds',
+  'Revenge of the Pink Panther',
+  'Star Wars: Episode I - The Phantom Menace',
+  'Star Wars: Episode II - Attack of the Clones',
+  'Star Wars: Episode III - Revenge of the Sith'
+])
 // Ne pas modifier l'export
 module.exports = searchWordFilter;
