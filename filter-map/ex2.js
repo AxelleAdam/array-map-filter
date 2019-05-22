97222b54151dc03a9e5d766d7b74d16cff2f3bca
Axelle Adamkiewicz <axelle.adamkiewicz@gmail.com>
2019-05-22 12:02:49 +0200

    Adding filter-map exercices.

--- filter-map/ex2.js
+++ filter-map/ex2.js
@@ -1,13 +1,10 @@
 /* Array.prototype.map ET filter - Exercice 2
-
 Il s'agit d'écrire une fonction getCampusesTeachingReact, qui reçoit en argument un tableau
 d'objets décrivant des campus de la Wild Code School. Chaque campus a ces propriétés:
 - city: ville où se trouve le campus
 - curriculums: les cursus proposés dans ce campus (PHP/Symfony, JS/React, JS/Angular, etc.)
-
 En utilisant filter puis map, la fonction doit renvoyer les villes des campus qui proposent
 un cursus JS/React.
-
 Tableau en entrée:
 [
   { city: 'Bordeaux', curriculums: ['PHP/Symfony', 'JS/React'] },
@@ -18,13 +15,48 @@ Tableau en entrée:
   { city: 'Reims', curriculums: ['JS/React'] },
   { city: 'Toulouse', curriculums: ['JEE/Android', 'JS/React'] }
 ]
-
 Sortie attendue:
 ['Bordeaux', 'Lille', 'Marseille', 'Reims', 'Toulouse']
-
 */
 
 function getCampusesTeachingReact(campuses) {
-}
+  return (campuses.filter((campus) => {
+    if (campus.curriculums.includes('JS/React')) {
+      return campus;
+    }
+  }).map((ville) => {
+    return ville.city;
+  }));
+};
+
+getCampusesTeachingReact([{
+    city: 'Bordeaux',
+    curriculums: ['PHP/Symfony', 'JS/React']
+  },
+  {
+    city: 'La Loupe',
+    curriculums: ['JS/Angular']
+  },
+  {
+    city: 'Lille',
+    curriculums: ['PHP/Symfony', 'JS/React']
+  },
+  {
+    city: 'Marseille',
+    curriculums: ['JS/React']
+  },
+  {
+    city: 'Orléans',
+    curriculums: ['PHP/Symfony']
+  },
+  {
+    city: 'Reims',
+    curriculums: ['JS/React']
+  },
+  {
+    city: 'Toulouse',
+    curriculums: ['JEE/Android', 'JS/React']
+  }
+])
 
 module.exports = getCampusesTeachingReact;
